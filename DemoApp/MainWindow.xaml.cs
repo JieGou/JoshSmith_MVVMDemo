e2bf79292e6729debf19894f805d2cc7d11f9377
陈杰 <jiegou04@gmail.com>
2020-08-19 01:26:44 +0800

    Add TODO Task

--- DemoApp/MainWindow.xaml.cs
+++ DemoApp/MainWindow.xaml.cs
@@ -6,5 +6,15 @@
         {
             InitializeComponent();
         }
+
+        //TODO 可以改善的内容，加深理解
+        /*
+        1、标题的修改 带*号
+        2、标题的拖动
+        3、增加编辑功能
+        4、删除用户
+        5、添加toolTip
+        6、使用fluttervalidation去修改验证
+        */
     }
 }
